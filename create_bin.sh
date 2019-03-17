ea61b6e1feb757c78e923aca2f247868334c2b17
Unknown <uy.nathalie@yahoo.com>
2019-03-17 18:00:43 +0100

    Changement lien pour récupérer le bin

--- create_bin.sh
+++ create_bin.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
- curl -sSL https://goo.gl/6wtTN5 | bash -s 1.4.0
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- -s 1.4.0
+
 
 exit 0
